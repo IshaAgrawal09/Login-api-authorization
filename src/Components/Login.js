8c59400d38fe3310cae8d82d852f782a2ea127d5
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-09-27 17:46:44 +0530

    Login Page with Authorization

---
+++ src/Components/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        authorization:
+          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiMSIsInJvbGUiOiJhcHAiLCJpYXQiOjE1MzkwNTk5NzgsImlzcyI6Imh0dHBzOlwvXC9hcHBzLmNlZGNvbW1lcmNlLmNvbSIsImF1ZCI6ImV4YW1wbGUuY29tIiwibmJmIjoxNTM5MDU5OTc4LCJ0b2tlbl9pZCI6MTUzOTA1OTk3OH0.GRSNBwvFrYe4H7FBkDISVee27fNfd1LiocugSntzxAUq_PIioj4-fDnuKYh-WHsTdIFMHIbtyt-uNI1uStVPJQ4K2oYrR_OmVe5_zW4fetHyFmoOuoulR1htZlX8pDXHeybRMYlkk95nKZZAYQDB0Lpq8gxnTCOSITTDES0Jbs9MENwZWVLfyZk6vkMhMoIAtETDXdElIdWjP6W_Q1kdzhwqatnUyzOBTdjd_pt9ZkbHHYnv6gUWiQV1bifWpMO5BYsSGR-MW3VzLqsH4QetZ-DC_AuF4W2FvdjMRpHrsCgqlDL4I4ZgHJVp-iXGfpug3sJKx_2AJ_2aT1k5sQYOMA",
+      },
+      //   body: JSON.stringify({
+      //     username,
+      //     password,
+      //   }),
+    };
+    fetch(
+      `https://fbapi.sellernext.com/user/login?username=${username}&password=${password}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+
+      .then((actualData) => {
+        if (!actualData.success) {
+          throw new Error("Invalid UserName or Password");
+        }
+        localStorage.setItem("token", actualData.data.token);
+        navigate("/home");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+  
+  return (
+    <div>
+      <form>
+        <div>
+          Enter UserName: &nbsp;
+          <input
+            required
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          Enter Password: &nbsp;&nbsp;
+          <input
+            required
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" onClick={submit} />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
